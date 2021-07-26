288c8118bf466452b25a17690811a3ab291ba1f7
Haihao Sun <haihaosun@Haihaos-MBP.guest.corp.microsoft.com>
2021-07-26 14:55:42 +0800

    with some dp

--- 3-Special-Topics/31-Common-Techniques-Basics/06-productExceptSelf.cpp
+++ 3-Special-Topics/31-Common-Techniques-Basics/06-productExceptSelf.cpp
@@ -1,1 +1,23 @@
 // https://blueblazin.github.io/codefighter/commontechniques/2017/12/19/productExceptSelf.html
+
+
+
+// 
+// s2 = a + b
+// p2 = ab
+
+// s3 = ab + bc + ac = s2 * num[3] + p2
+// p3 = abc = p2 * num[3]
+
+def productExceptSelf(nums, m):
+    s = [0]
+    p = [1]
+    for num in nums:
+        sn = (s[-1] * num + p[-1]) % m
+        pn = (p[-1] * num) % m
+        s.append(sn)
+        p.append(pn)
+    return s[-1]
+        
+
+
